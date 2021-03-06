833b830c198173949e970da8c7bd594da189bbb8
nekit <n>
2021-03-06 15:23:33 +0300

    адаптивность

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-alert(document.getElementsByClassName(email))
+// sort
+$(document).ready(function () {
+    $('.work__button').click(function () {
+        const value = $(this).attr('data-filter')
+        if (value == 'all') {
+            $('.item-box').show('1000')
+        }
+        else {
+            $('.item-box').not('.' + value).hide('1000')
+            $('.item-box').filter('.' + value).show('1000')
+        }
+    })
+    $('.work__button').click(function () {
+        $(this).addClass('active').siblings().removeClass('active');
+    })
+})
